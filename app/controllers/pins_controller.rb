842dbf83efdcd9f8dcc30f7d74b4ac5b0dd2dec4
Peter Madara <petermadara@hotmail.com>
2014-05-16 23:01:44 -0400

    changes through Pins Users and Associations

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
     def index
     @pins = Pin.all
@@ -9,14 +10,14 @@ class PinsController < ApplicationController
   end
 
     def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
     def edit
   end
 
     def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
 
       if @pin.save
         redirect_to @pin, notice: 'Pin was successfully created.'
@@ -31,7 +32,7 @@ class PinsController < ApplicationController
         redirect_to @pin, notice: 'Pin was successfully updated.'
       else
         render action: 'edit' 
-      end
+      send
     end
 
   
@@ -50,4 +51,5 @@ class PinsController < ApplicationController
     def pin_params
       params.require(:pin).permit(:description)
     end
+  end
 end
